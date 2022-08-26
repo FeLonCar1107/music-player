94ea220cab756a7c06bd8e1407620cf63b42f31c
FeLonCar1107 <pipe20001107@gmail.com>
2022-08-26 17:01:37 -0500

    maquetacion

---
+++ app.js
@@ -0,0 +1,35 @@
+const songList = [
+    {
+        title: "Selena Browns",
+        file: "Selena Dreaming Of You.mp3",
+        cover: "cancion1.jpg"
+    },
+    {
+        title: "Happier",
+        file: "Bastille Happier.mp3",
+        cover: "cancion2.jpg"
+    },
+    {
+        title: "All Aroun the World",
+        file: "ATC Around The World.mp3",
+        cover: "cancion3.jpg"
+    },
+    {
+        title: "Humility",
+        file: "Gorillaz Humility.mp3",
+        cover: "cancion4.jpg"
+    },
+    {
+        title: "New Rules",
+        file: "Dua lipa New Rules.mp3",
+        cover: "cancion4.jpg"
+    },
+]
+
+// 
+const user = document.querySelector(".user");
+const search = document.querySelector(".search");
+const btn_logOut = document.querySelector(".logOut");
+const album = document.querySelector(".albumItem");
+const song = document.querySelector(".song");
+
